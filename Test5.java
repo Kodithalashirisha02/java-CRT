b1a44749330e7fa306bfc88ced26aa3199fa2a41
Kodithalashirisha02 <shirishak236@gmail.com>
2024-07-02 09:32:39 +0530

    string

---
+++ Test5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Test5
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str="java is simple language";
+System.out.println(str.contains("simple"));
+System.out.println(str.contains("python"));
+System.out.println(str.startsWith("J"));
+System.out.println(str.endsWith("age"));
+System.out.println(str.startsWith("K"));
+}
+}
